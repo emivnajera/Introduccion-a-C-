e2ddd945a90c0ffa05b6bfd39289ee67e24af743
emivnajera <emivelasquez2000@hotmail.com>
2022-11-25 14:36:34 -0600

    added arrays project.

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] coffeTypes;
+            float[] coffeValues;
+
+            coffeTypes = new string[] { "Expresso", "Largo", "Filtrado", "Latte" };
+            coffeValues = new float[] { 1.2f, 1.5f, 5.0f, 5, 5f };
+
+            coffeTypes[1] = "Lungo";
+
+            for(int i = 0; i<4;i++)
+            {
+                Console.WriteLine(coffeTypes[i] + "Coffe $" + coffeValues[i]);
+            }
+        }
+    }
+}
