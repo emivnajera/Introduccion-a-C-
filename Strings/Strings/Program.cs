06c64da1c49e135396ab9ae10cb2c2480e91c4bf
emivnajera <emivelasquez2000@hotmail.com>
2022-11-25 14:29:45 -0600

    added strings file.

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = 170;
+            int age = 22;
+            string name = "Emiliano Velasquez";
+            string placeholder = "La informacion es: ";
+            string information = placeholder + "\n User name: " + name +"\n User age: " + age + "\n User Height: " +height;
+            Console.WriteLine(information);
+        }
+    }
+}
