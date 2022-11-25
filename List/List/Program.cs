e612d09d6d03f5ba93594f04718562337ab2a744
emivnajera <emivelasquez2000@hotmail.com>
2022-11-25 14:43:14 -0600

    added lists project.

---
+++ List/List/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+List<string> list = new List<string>();
+
+list.Add("UNO");
+list.Add("DOS");
+list.Add("TRES");
+list.Add("CUATRO");
+
+
+for(int i = 0; i < list.Count; i++)
+{
+    Console.WriteLine(list[i]);
+}
+
+list.Remove("TRES");
+list.RemoveAt(0);
+
+Console.WriteLine();
+
+for (int i = 0; i < list.Count; i++)
+{
+    Console.WriteLine(list[i]);
+}
