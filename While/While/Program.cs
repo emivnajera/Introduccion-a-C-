3678e2afe35e356fd6d2d5827a8393fb7c900b25
emivnajera <emivelasquez2000@hotmail.com>
2022-12-05 13:24:57 -0600

    added while file.

---
+++ While/While/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+           while (i <= 50){
+                Console.WriteLine(i);
+                i = i + 10;
+           }
+
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j);
+                j = j + 10;
+            }while(j <= 50);
+        }
+    }
+}
