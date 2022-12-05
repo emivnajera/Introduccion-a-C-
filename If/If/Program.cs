abd78242e337e70cda8914efb124e85d3e6c5a29
emivnajera <emivelasquez2000@hotmail.com>
2022-12-05 13:17:11 -0600

    added if file.

---
+++ If/If/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace If
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anyValue = 6;
+            string message = "";
+            if(anyValue == 7)
+            {
+                message = "Es 7";
+            }
+            else if(anyValue == 14)
+            {
+                message = "Es 14";
+            }
+            else
+            {
+                message = "No es 7 ni 14";
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
