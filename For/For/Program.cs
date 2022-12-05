5d16c62d99b2310b99a46a9a51e66a5f540d7038
emivnajera <emivelasquez2000@hotmail.com>
2022-12-05 13:21:29 -0600

    added for file.

---
+++ For/For/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 50; i = i+10)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
